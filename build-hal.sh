d21c8082bf0acfb2c8000bdbd3daf6a718ad9e7e
MaxRegner <59399856+MaxRegner@users.noreply.github.com>
2022-12-15 13:39:53 +0100

    Update build-hal.sh

--- build-hal.sh
+++ build-hal.sh
@@ -7,11 +7,5 @@ export USE_CCACHE=1
 breakfast aosp_f5321-eng
 make -j$(nproc) mkbootimg
 make -j$(nproc) hybris-boot
-make -j$(nproc) halium-boot
-make -j$(nproc) recoveryimage
-make -j$(nproc) systemimage 
+ 
 
-echo "md5sum halium-boot.img and system.img"
-md5sum $ANDROID_ROOT/out/target/product/rolex/halium-boot.img
-md5sum $ANDROID_ROOT/out/target/product/rolex/recovery.img
-md5sum $ANDROID_ROOT/out/target/product/rolex/system.img
